5346159b9100df879f4fbc370e311a9ca18d735b
rodrigo7584 <waboo@waboo.com.br>
2025-06-09 11:35:22 -0300

    upload

--- back/server.js
+++ back/server.js
@@ -18,7 +18,8 @@ app.use(
 );
 
 // Middleware para ler JSON
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 // Rotas
 app.use("/api/uploadDual", uploadDualRoutes);
--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  serverActions: {
-    bodySizeLimit: "10mb", // Define o limite para 10MB
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
